d334a652184cf76211153f341538255b5fbaf313
atlaswacera-cyber <atlaswacera@gmail.com>
2026-07-07 12:03:14 -0500

    Validate signin request fields

--- controllers/users.js
+++ controllers/users.js
@@ -60,6 +60,10 @@ const createUser = (req, res) => {
 const login = (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+  }
+
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
